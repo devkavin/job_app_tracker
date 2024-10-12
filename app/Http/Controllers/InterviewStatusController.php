e722fce37925b752ddca9e09cdf13d7fa0fe2aa1
devkavin <kavindra.senanayake@gmail.com>
2024-10-12 19:41:11 +0530

    Add InterviewStatusController and InterviewStatus model

---
+++ app/Http/Controllers/InterviewStatusController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InterviewStatus;
+use Illuminate\Http\Request;
+
+class InterviewStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InterviewStatus $interviewStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InterviewStatus $interviewStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InterviewStatus $interviewStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InterviewStatus $interviewStatus)
+    {
+        //
+    }
+}
